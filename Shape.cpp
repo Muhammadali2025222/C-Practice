aa1f7f0aa2bc7130ac9681d819686b5a72b38244
Muhammadali2025222 <muhammadali2025222@gmail.com>
2024-12-10 18:53:05 +0500

    change comment in shape and shapefactory

--- Shape.cpp
+++ Shape.cpp
@@ -12,9 +12,7 @@ class Shape : public Area, public Volume, public Input
 // 2 . To store and to calculate the computable dimensions . 
 // 3 . Drawing the shape .
 
-// This class doesnot follows the Open Closed Princple .
-// Reason : Because of tight coupling 
-// in other words if we want to draw a certain shape we must take user's input for calculating the dimensions. 
+// This class follows the Open Closed Princple . 
 
 // This class follows the Interface Segregation Principle .
 
